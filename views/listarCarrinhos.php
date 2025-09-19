972d860efa6de3c2b3ee3c4c6418c73046a2d7cc
Vitor Venante <vitor.venante@gmail.com>
2025-09-18 22:16:49 -0300

    correcao listarCarrinhos.php

--- views/listarCarrinhos.php
+++ views/listarCarrinhos.php
@@ -8,15 +8,26 @@
 <body>
     <h1>Carrinhos Adquiridos</h1>
 
-    <!-- CORRIGIR LISTAR -->
-    <?php foreach ($carrinhos as $e): ?>
-        <tr>
-            <td><?php htmlspecialchars($c['modelo'])    ?></td>
-            <td><?php htmlspecialchars($c['categoria']) ?></td>
-            <td><?php htmlspecialchars($c['numero'])    ?></td>
-            <td><?php $c['criado_em']                   ?></td>
-        </tr>
-    <?php endforeach ?>
+    <table border="1" cellpadding="10">
+        <thead>
+            <tr>
+                <th>Modelo</th>
+                <th>Categoria</th>
+                <th>Numero</th>
+            </tr>
+        </thead>
+        <tbody>
+             <?php foreach ($carrinhos as $c): ?>
+                <tr>
+                    <td><?= htmlspecialchars($c['modelo'])    ?></td>
+                    <td><?= htmlspecialchars($c['categoria']) ?></td>
+                    <td><?= htmlspecialchars($c['numero'])    ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+
+    <a href="views/cadastroCarrinho.php">Cadastrar</a>
 
 </body>
 </html>
